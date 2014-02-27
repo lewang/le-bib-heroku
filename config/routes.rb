f3252975b81eb2528d6b59ad1c48eedf7a539f36
Le Wang <le.wang@agworld.com.au>
2014-02-26 19:11:36 -0500

    fix root route typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 BibHeroku::Application.routes.draw do
-  root 'welcome#index'
+  root 'pages#welcome'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
